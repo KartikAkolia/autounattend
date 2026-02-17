9467b708a272fe1f8bf9d43638f80d231c62002a
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2026-02-17 13:28:03 +0000

    Update URLs for autounattend.xml and oscdimg.exe

--- autounattend.ps1
+++ autounattend.ps1
@@ -14,8 +14,8 @@ $Drive = (Get-DiskImage -ImagePath $ISO | Mount-DiskImage | Get-Volume).DriveLet
 New-Item Sources -ItemType Directory | Out-Null
 Copy-Item -Path "$Drive\*" -Destination Sources -Recurse -Force
 
-irm https://github.com/GabiNun/autounattend/raw/main/autounattend.xml -Out Sources\autounattend.xml
-irm https://github.com/GabiNun/autounattend/raw/main/oscdimg.exe -Out oscdimg.exe
+irm https://github.com/KartikAkolia/autounattend/raw/main/autounattend.xml -Out Sources\autounattend.xml
+irm https://github.com/KartikAkolia/autounattend/raw/main/oscdimg.exe -Out oscdimg.exe
 
 .\oscdimg.exe "-bSources\efi\microsoft\boot\efisys.bin" -u2 Sources autounattend.iso
 
